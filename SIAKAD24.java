6e53af491e0fa89e61f0dd3830f8b3309f6726c9
Sharren <sharrenepf@gmail.com>
2024-11-13 17:56:34 +0700

    percobaan 4

---
+++ SIAKAD24.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SIAKAD24 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = input.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = input.nextInt();
+
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
+                nilai[i][j] = input.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata mahasiswa ke-" + (i + 1) + ": " + totalPerSiswa / jumlahMatkul);
+        }
+
+        System.out.println("\n=========================================");
+        System.out.println("Rata-rata nilai setiap Mata Kuliah:");
+
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / jumlahSiswa);
+        }
+        
+        input.close();
+    }
+}
